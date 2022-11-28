da4945f20267ce82f4b8c1cc3aea39377a528e9d
Roman <romanuch06.88@gmail.com>
2022-11-28 11:11:23 +0200

    Create App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,37 @@
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Appbar from './AppBar/AppBar';
+
+
+const HomePage = lazy(() =>
+  import('./HomePage/HomePage')
+);
+const MoviesPage = lazy(() =>
+  import('../views/MoviesPage')
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    './MovieDetailsPage/MovieDetailsPage')
+);
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+const Cast = lazy(() => import('./Cast/Cast'));
+
+export const App = () => {
+  return (
+    <>
+      <Appbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route index element={<HomePage />}></Route>
+          <Route path="/movies" element={<MoviesPage />}></Route>
+          <Route path="/movies/:movieId/" element={<MovieDetailsPage />}>
+            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+            <Route path="/movies/:movieId/cast" element={<Cast />} />
+          </Route>
+
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
+};
